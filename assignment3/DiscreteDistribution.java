68db2d868761f90d90c7fa6ce940eb687ad2417a
= <xquinnma@gmail.com>
2021-06-18 01:40:45 -0400

    assignment3 first two

---
+++ assignment3/DiscreteDistribution.java
@@ -0,0 +1,35 @@
+public class DiscreteDistribution
+{
+        public static void main (String[] args)
+        {
+                int  m = Integer.parseInt(args[0]);
+                int[] n = new int[m];
+                int l = args.length;
+                int total = 0;
+                
+                //read in inputs and create an array for it 
+                for (int i = 1; i < l; i++)
+                {
+                        n[i] = Integer.parseInt(args[i]);
+                        total += n[i];
+                 }
+               
+                for (int j = 0; j < m; j ++) 
+                {
+                    int r = (int)(total*Math.random());
+                    int sum = 0, event = -1; 
+                    for (int i = 0; i < m && sum <=r; i++)
+                    {
+                            sum +=n[i];
+                            event = i;
+                    }
+                    System.out.print(event + " ");
+                    
+                    if((j+1) >= 25 && (j+1) % 25 == 0)  System.out.println();
+                }   
+                    System.out.println();
+        }
+}
+
+
+
